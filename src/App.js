15f233ddadecc3552151162c16291df3e3511d86
flatcalm <a_abnc@naver.com>
2023-06-12 15:49:10 +0900

    # 2. 이벤트와 스타일

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import { Fragment } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Hello from './Hello';
 import SayHello from './SayHello';
+import Hello from './Hello';
+import ChangeEvent from './component/chap01_event/ChangeEvent';
+import ClickEvent from './component/chap01_event/ClickEvent';
+import SubmitEvent from './component/chap01_event/SubmitEvent';
 
 // 리액트에서는 하나의 컴포넌트가 여러개의 엘리먼트를 반환할 수 있습니다.
 // JSX를 작성할 때 return 문 안에 반드시 하나의 최상위 태그가 있어야 합니다.
@@ -28,7 +31,7 @@ function App() {
   // {} 안에서 함수의 호출문이나 변수 참조는 가능.
   return (
     <>
-      { looping() }
+      <SubmitEvent />
     </>
   );
 }
